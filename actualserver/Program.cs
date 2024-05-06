36c543c975afcd99637118f35331c2728d6ec33c
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2024-05-06 09:34:53 +0200

    Přidejte soubory projektu.

---
+++ actualserver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace actualserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1");
+        }
+    }
+}
